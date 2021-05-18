62496c11dd590bff3face5e7a031e8a8b4a74b60
aggale <annagrgale@gmail.com>
2021-05-18 08:13:57 -0400

    Initial landing page

---
+++ src/Landing/Landing.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Landing.css";
+import logo from "../assets/images/Recipes-small-logo.png";
+import browseButton from "../assets/images/browse-button.png";
+import searchButton from "../assets/images/search-button.png";
+
+const Landing = () => {
+  return (
+    <div className="centered column">
+      <img src={logo} alt="Recipes Period logo" />
+      <img src={browseButton} alt="Browse" className="image-button" />
+      <img src={searchButton} alt="Search" className="image-button" />
+    </div>
+  );
+};
+
+export default Landing;
