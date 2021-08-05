be75da27c195d7033ddd42e4aae381b2f505dc29
aggale <annagrgale@gmail.com>
2021-08-05 10:01:10 -0400

    Trying things for landing page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Landing from "./Landing/Landing";
+import Header from "./Header/Header";
 
 class App extends Component {
   render() {
@@ -11,6 +12,9 @@ class App extends Component {
         <Switch>
           <Route exact path="/">
             <Landing />
+          </Route>
+          <Route path="/browse">
+            <Header />
           </Route>
         </Switch>
       </Router>
