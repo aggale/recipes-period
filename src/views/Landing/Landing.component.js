ee34ed5607f5cd54b9f77072ebe621fd36db9226
unknown <annagrgale@gmail.com>
2021-08-06 14:32:33 -0400

    Added routing to buttons on landing page

--- src/views/Landing/Landing.component.js
+++ src/views/Landing/Landing.component.js
@@ -1,15 +1,25 @@
 import React from "react";
+import { useHistory } from "react-router";
+
 import "./Landing.css";
+
 import logo from "../../assets/images/Recipes-small-logo.png";
 import browseButton from "../../assets/images/browse-button.png";
 import searchButton from "../../assets/images/search-button.png";
 
 const LandingPage = () => {
+
+  const history = useHistory();
+
+  const onNavigate = (route) => () => {
+    history.push(route);
+  }
+
   return (
     <div className="centered column">
       <img src={logo} alt="Recipes Period logo" />
-      <img src={browseButton} alt="Browse" className="image-button" />
-      <img src={searchButton} alt="Search" className="image-button" />
+      <img src={browseButton} alt="Browse" className="image-button" onClick={onNavigate('browse')} />
+      <img src={searchButton} alt="Search" className="image-button" onClick={onNavigate('search')} />
     </div>
   );
 };
