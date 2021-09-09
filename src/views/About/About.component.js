424784183c55186fccb63ac2ef666a7b504b4897
unknown <annagrgale@gmail.com>
2021-09-09 10:18:32 -0400

    Visual cleanup of the about page

--- src/views/About/About.component.js
+++ src/views/About/About.component.js
@@ -6,8 +6,9 @@ import './About.styles.css';
 
 const AboutPage = () => (
     <TranslucentBox className='transparent-container'>
-        <h1 className="headline">Recipes. is about recipes. Period.</h1>
-        <p className="medium-text">No more of the endless scroll searching for an ingredient list. We bring you recipes and nothing else so that you can get cooking.</p>
+        <h1 className="about-text headline">Recipes. is about recipes. Period.</h1>
+        <p className="about-text medium-text">No more of the endless scroll searching for an ingredient list. We bring you recipes and nothing else so that you can get cooking.</p>
+        <p className="about-text small-text">Enjoy main dishes, sides, desserts, and more that prioritize healthy, whole ingredients that fuel your body without sacrificing flavor.</p>
     </TranslucentBox>
 );
 
