ab62eb02beab32018fc8694f34a9a2ecebadf4cd
aggale <annagrgale@gmail.com>
2021-09-18 09:01:31 -0400

    Updated summary cards to a more advanced ui look

--- src/components/containers/SummaryCard/SummaryCard.component.js
+++ src/components/containers/SummaryCard/SummaryCard.component.js
@@ -1,30 +1,69 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import Card from '../Card/Card.component';
+import Card from "../Card/Card.component";
 
-import './SummaryCard.styles.css';
+import "./SummaryCard.styles.css";
 
-const SummaryCard = ({id, title, description, difficulty, time, ingredients, imageUrl}) => {
-    return (
-        <div className='summary-card'>
-            <Card>
-                {imageUrl && (
-                    <Link to={`/recipes/${id}`}>
-                        <img className="card-image summary-card-image" src={imageUrl} alt={`${title}`} />
-                    </Link>
-                )}
-                <h1>{title}</h1>
-                <p className="summary-card-description">{description}</p>
-                <div className="summary-card-extra-info">
-                    <p><span className="bold">Difficulty: </span>{difficulty}</p>
-                    <p><span className="bold">Time: </span>{time}</p>
-                    <p><span className="bold">Ingredients: </span>{ingredients}</p>
-                </div>                
-            </Card>
-        </div>
-        
-    );
-}
+const SummaryCard = ({
+  id,
+  title,
+  description,
+  difficulty,
+  servings,
+  time,
+  ingredients,
+  imageUrl
+}) => {
+  return (
+    <div className="summary-card">
+      <Card>
+        <aside>
+          {imageUrl && (
+            <Link to={`/recipes/${id}`}>
+              <img
+                className="card-image summary-card-image"
+                src={imageUrl}
+                alt={`${title}`}
+              />
+            </Link>
+          )}
+        </aside>
+        <article>
+          <h2>{title}</h2>
+          <ul className="card-recipe-info">
+            <li>
+              <span className="icon">
+                <i className="fas fa-user-friends"></i>
+              </span>
+              <span>{servings}</span>
+            </li>
+            <li>
+              <span className="icon icon-clock">
+                <i className="far fa-clock"></i>
+              </span>
+              <span>{time}</span>
+            </li>
+            <li>
+              <span className="icon icon-level">
+                <span className="icon">
+                  <i className="fas fa-level-up-alt"></i>
+                </span>
+              </span>
+              <span>{difficulty}</span>
+            </li>
+          </ul>
+          <p className="summary-card-description">{description}</p>
+          <div className="summary-card-extra-info">
+            <p>
+              <span className="bold">Ingredients: </span>
+              {ingredients}
+            </p>
+          </div>
+        </article>
+      </Card>
+    </div>
+  );
+};
 
 export default SummaryCard;
--- src/firebase/firebase-utils.js
+++ src/firebase/firebase-utils.js
@@ -1,78 +1,102 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/storage';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA13ED-Z8x5FLpeEzdSm-VrifHOypF6kgc",
-    authDomain: "recipes-period.firebaseapp.com",
-    projectId: "recipes-period",
-    storageBucket: "recipes-period.appspot.com",
-    messagingSenderId: "187803724429",
-    appId: "1:187803724429:web:974d3f5795c77a3a9f90c4",
-    measurementId: "G-69H3NH0GJV"
+  apiKey: "AIzaSyA13ED-Z8x5FLpeEzdSm-VrifHOypF6kgc",
+  authDomain: "recipes-period.firebaseapp.com",
+  projectId: "recipes-period",
+  storageBucket: "recipes-period.appspot.com",
+  messagingSenderId: "187803724429",
+  appId: "1:187803724429:web:974d3f5795c77a3a9f90c4",
+  measurementId: "G-69H3NH0GJV"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const firestore = firebase.firestore();
+
+/**
+ * Getting data from firestore and packaging it into the JSON object I need.
+ * @param {*} recipes handle for the recipe collection
+ * @returns JSONy object representing my data
+ */
+export const convertRecipesSnapshotToMap = recipes => {
+  return recipes.docs
+    .map(doc => {
+      const {
+        title,
+        description,
+        difficulty,
+        ingredients,
+        servings,
+        steps,
+        time,
+        image,
+        id
+      } = doc.data();
+
+      return {
+        title,
+        description,
+        difficulty,
+        ingredients,
+        servings,
+        steps,
+        time,
+        image,
+        id
+      };
+    })
+    .reduce(
+      (recipesObj, recipe) => ({ ...recipesObj, [recipe.id]: recipe }),
+      {}
+    );
+};
+
+/**
+ * Getting data from firestore and packaging it into the JSON object I need.
+ * @param {*} recipes handle for the recipe collection
+ * @returns JSONy object representing my data
+ */
+export const convertRecipeSnapshotToObject = recipeSnapshot => {
+  const {
+    title,
+    description,
+    difficulty,
+    ingredients,
+    steps,
+    time,
+    image,
+    id
+  } = recipeSnapshot.data();
+
+  return {
+    title,
+    description,
+    difficulty,
+    ingredients,
+    steps,
+    time,
+    image,
+    id
   };
+};
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
-  export const firestore = firebase.firestore();
-
-  /**
-   * Getting data from firestore and packaging it into the JSON object I need.
-   * @param {*} recipes handle for the recipe collection
-   * @returns JSONy object representing my data
-   */
-  export const convertRecipesSnapshotToMap = (recipes) => {
-      return recipes.docs.map(doc => {
-          const { title, description, difficulty, ingredients, steps, time, image, id } = doc.data();
-
-          return {
-              title,
-              description,
-              difficulty,
-              ingredients,
-              steps,
-              time,
-              image,
-              id
-          };
-      })
-      .reduce((recipesObj, recipe) => ({...recipesObj, [recipe.id]: recipe}), {});
+/**
+ * Get a url based on the name of the image in Firestore
+ * Reference: https://medium.com/@sultanbutt820/react-native-image-upload-retrieve-delete-from-firebase-cloud-storage-ios-android-e05c7cdbf1d2
+ */
+export const getImageUrl = async imageName => {
+  if (imageName) {
+    const imageRef = firebase.storage().ref("/" + imageName);
+    try {
+      return await imageRef.getDownloadURL();
+    } catch (e) {
+      console.error("Error getting image url: ", e);
+    }
   }
 
-  /**
-   * Getting data from firestore and packaging it into the JSON object I need.
-   * @param {*} recipes handle for the recipe collection
-   * @returns JSONy object representing my data
-   */
-   export const convertRecipeSnapshotToObject = (recipeSnapshot) => {
-        const { title, description, difficulty, ingredients, steps, time, image, id } = recipeSnapshot.data();
-
-        return {
-            title,
-            description,
-            difficulty,
-            ingredients,
-            steps,
-            time,
-            image,
-            id
-        };
-    }
-
-  /**
-   * Get a url based on the name of the image in Firestore
-   * Reference: https://medium.com/@sultanbutt820/react-native-image-upload-retrieve-delete-from-firebase-cloud-storage-ios-android-e05c7cdbf1d2
-   */
-export const getImageUrl = async (imageName) => {
-    if (imageName) {
-        const imageRef = firebase.storage().ref('/' + imageName);
-        try {
-            return await imageRef.getDownloadURL();
-        } catch (e) {
-            console.error('Error getting image url: ', e)
-        }
-    }
-
-    return null;
-}
+  return null;
+};
