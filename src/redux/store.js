6bd981f5700fb3c0c04e447b8d197f6bcfa620b5
aggale <annagrgale@gmail.com>
2021-09-23 13:05:40 -0400

    CSS adjustments for smaller screens

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit'
-import logger from 'redux-logger';
-import thunk from 'redux-thunk'
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 import recipesReducer from "./recipes/recipes.slice";
 
 const middleware = [thunk];
 
-if (process.env.NODE_ENV === 'development') {
-    middleware.push(logger);
+if (process.env.NODE_ENV === "development") {
+  middleware.push(logger);
 }
 
 const store = configureStore({
-    reducer: {
-        recipes: recipesReducer
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middleware),
+  reducer: {
+    recipes: recipesReducer
+  },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(middleware)
 });
 
 export default store;
